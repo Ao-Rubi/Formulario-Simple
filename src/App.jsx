9e99a90742d8952680a3d61622f2e24be77f32e9
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-08-14 18:15:12 -0300

    Acomodado de archivos y maquetado del formulario

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Llene el formulario</h1>
+      
+    </div>
+  );
+}
+
+export default App;
