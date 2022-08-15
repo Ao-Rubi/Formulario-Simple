98e29cacd625fd292b3579b55b90c05a826effe6
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-08-15 00:26:04 -0300

    Validacion por encima puesta, valida con patrones

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,32 +2,45 @@ import React, { useEffect, useState } from 'react';
 import { Form, Button} from 'react-bootstrap';
 
 const Formulario = () => {
+    const [validated, setValidated] = useState(false);
     const [nombre, setNombre] = useState("");
     const [apellido, setApellido] = useState("");
     const [DNI, setDNI] = useState("");
     const [email, setEmail] = useState("");
 
+    const handleSubmit = (e) => {
+
+        const form = e.currentTarget;
+
+        if (form.checkValidity() === false) {
+            e.preventDefault();
+            e.stopPropagation();
+        }
+
+        setValidated(true);
+    }
+
     return (
         <div className='py-4 px-3'>
-            <Form>
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
                 <Form.Group className="mb-3">
                     <Form.Label>Nombre</Form.Label>
-                    <Form.Control type="text" placeholder="Nombre" />
+                    <Form.Control onChange={(e)=> { setNombre(e.target.value)}} type="text" placeholder="Nombre" required/>
                 </Form.Group>
 
                 <Form.Group className="mb-3">
                     <Form.Label>Apellido</Form.Label>
-                    <Form.Control type="text" placeholder="Apellido" />
+                    <Form.Control type="text" placeholder="Apellido" required/>
                 </Form.Group>
 
                 <Form.Group className="mb-3">
                     <Form.Label>DNI</Form.Label>
-                    <Form.Control type="text" placeholder="DNI" />
+                    <Form.Control type="number" placeholder="DNI" required/>
                 </Form.Group>
 
                 <Form.Group className="mb-3">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="Email" />
+                    <Form.Control type="email" placeholder="Email" required/>
                 </Form.Group>
 
                 <Button variant="primary" type="submit">Submit</Button>
