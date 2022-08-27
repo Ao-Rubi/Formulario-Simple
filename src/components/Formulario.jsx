a1471eeaa000e18d9a4d4ff7f823f198f04b03df
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-08-27 17:30:05 -0300

    Funcionamiento del formulario completado, agregar estilos para el usuario.

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import { Button} from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
 import { cantidadCaracteres, validarDNI, validarEmail } from './validaciones';
+import Form from 'react-bootstrap/Form';
 
 const Formulario = () => {
 
-    const nombre = document.getElementById("nombre");
-    const apellido = document.getElementById("apellido");
-    const DNI = document.getElementById("DNI");
-    const email = document.getElementById("email");
+    const [nombre, setNombre] = useState("");
+    const [apellido, setApellido] = useState("");
+    const [DNI, setDNI] = useState(0);
+    const [email, setEmail] = useState("");
 
-    const validacionCheck = () => {
-        if (cantidadCaracteres(2,15, nombre) && cantidadCaracteres(2,15, apellido) && validarDNI(DNI) && validarEmail(email)) {
+    const validacionCheck = (e) => {
+        e.preventDefault();
+        
+        if (cantidadCaracteres(nombre) && cantidadCaracteres(apellido) && validarDNI(DNI) && validarEmail(email)) {
             alert("Formulario completo")
         }else{
             alert("Campos vacios o incorrectos")
@@ -19,29 +22,29 @@ const Formulario = () => {
 
     return (
         <div className='py-4 px-3'>
-            <form onSubmit={validacionCheck}>
-                <div className="mb-3">
-                    <label htmlFor='nombre'>Nombre</label>
-                    <input id='nombre' className='form-control' onChange={()=> {cantidadCaracteres(2,15, nombre)}} type="text" placeholder="Nombre" required minLength={2} maxLength={15}/>
-                </div>
+            <Form onSubmit={validacionCheck}>
+                <Form.Group className="mb-3">
+                    <Form.Label htmlFor='nombre'>Nombre</Form.Label>
+                    <Form.Control id='nombre' className='form-control' onChange={(e)=> {setNombre(e.target.value)}} type="text" placeholder="Nombre" required minLength={2} maxLength={20}/>
+                </Form.Group>
 
-                <div className="mb-3">
-                    <label htmlFor='apellido'>Apellido</label>
-                    <input id='apellido' className='form-control' onChange={()=> {cantidadCaracteres(2,15, apellido)}} type="text" placeholder="Apellido" required  minLength={2} maxLength={15}/>
-                </div>
+                <Form.Group className="mb-3">
+                    <Form.Label htmlFor='apellido'>Apellido</Form.Label>
+                    <Form.Control id='apellido' className='form-control' onChange={(e)=> {setApellido(e.target.value)}} type="text" placeholder="Apellido" required  minLength={2} maxLength={15}/>
+                </Form.Group>
 
-                <div className="mb-3">
-                    <label htmlFor='DNI'>DNI</label>
-                    <input id='DNI' className='form-control' onChange={()=> {validarDNI(DNI)}} type="number" placeholder="DNI" required  minLength={8} maxLength={8}/>
-                </div>
+                <Form.Group className="mb-3">
+                    <Form.Label htmlFor='DNI'>DNI</Form.Label>
+                    <Form.Control id='DNI' className='form-control' onChange={(e)=> {setDNI(e.target.value)}} type="number" placeholder="DNI" required  minLength={8} maxLength={8}/>
+                </Form.Group>
 
-                <div className="mb-3">
-                    <label htmlFor='email'>Email</label>
-                    <input id='email' className='form-control' onChange={()=> {validarEmail(email)}} type="email" placeholder="Email" required />
-                </div>
+                <Form.Group className="mb-3">
+                    <Form.Label htmlFor='email'>Email</Form.Label>
+                    <Form.Control id='email' className='form-control' onChange={(e)=> {setEmail(e.target.value)}} type="email" placeholder="Email" required />
+                </Form.Group>
 
                 <Button variant="primary" type="submit">Submit</Button>
-            </form>
+            </Form>
         </div>
     );
 };
--- src/components/validaciones.js
+++ src/components/validaciones.js
@@ -1,42 +1,34 @@
 export function campoRequerido(input) {
-    if (input.value.trim().length > 0) {
-        input.className = "is-valid";
+    if (input.trim().length > 0) {
         return true;
     }else {
-        input.className = "is-invalid";
         return false;
     }
 }
 
 //Trim corta los espacios de adelante y de atras de un texto
-export function cantidadCaracteres(min, max, input) {
-    if (input.value.trim().length >= min && input.value.trim().length <= max) {
-        input.className = "form-control is-valid";
+export function cantidadCaracteres(input) {
+    if (input.length >= 3 && input.length <= 20) {
         return true;
     }else{
-        input.className = "form-control is-invalid";
         return false;
     }
 }
 
 export function validarDNI(input) {
     let patron = /^[\d]{1,2}[\d]{3,3}[\d]{3,3}$/;
-    if(patron.test(input.value.trim())) {
-        input.className = "form-control is-valid";
+    if(patron.test(input)) {
         return true;
     }else{
-        input.className = "form-control is-invalid";
         return false;
     }
 }
 
 export function validarEmail(input) {
     let patron = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
-    if(patron.test(input.value.trim())) {
-        input.className = "form-control is-valid";
+    if(patron.test(input)) {
         return true;
     }else{
-        input.className = "form-control is-invalid";
         return false;
     }
 }
